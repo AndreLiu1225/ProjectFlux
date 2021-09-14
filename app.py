48f8115398ddcad725d696abc300161fc2d98202
AndreLiu1225 <60701688+AndreLiu1225@users.noreply.github.com>
2021-09-14 21:39:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 # app.config['WHOOSH_BASE'] = 'whoosh'
 
-con = sqlite3.connect("Project_Flux//site.db", check_same_thread=False)
+con = sqlite3.connect("site.db", check_same_thread=False)
 cur = con.cursor()
 
 db = SQLAlchemy(app)
